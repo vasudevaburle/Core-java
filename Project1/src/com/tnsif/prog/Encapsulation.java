e0cb581b03a8a6926eae71ccec6031eb080ecf3e
Skill <Skill@10.0.0.121>
2023-09-22 14:33:16 +0530

    First Commit

---
+++ Project1/src/com/tnsif/prog/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tnsif.prog;
+
+public class Encapsulation {
+	//datamembers
+	public int serialno;
+	public string name;
+	public int age;
+	
+	//memberfunction
+	public void show() 
+	
+
+}
